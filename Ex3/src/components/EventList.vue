d8faddda133149c2e7360623badcbf6300cefa26
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2025-12-15 11:09:05 +0100

    feat:Ex3 base

---
+++ Ex3/src/components/EventList.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue";
+import EventCard from "./EventCard.vue";
+
+const events = ref([
+  {
+    id: 1,
+    time: '18:00',
+    date: '2025-12-10',
+    title: 'Soirée jeux de société',
+  },
+  {
+    id: 2,
+    time: '14:30',
+    date: '2025-12-15',
+    title: 'Atelier Vue 3',
+  },
+  {
+    id: 3,
+    time: '09:00',
+    date: '2025-12-20',
+    title: 'Petit-déjeuner de classe',
+  },
+])
+</script>
+
+<template>
+  <div class="event-container" v-for="myEvent in events" :key="myEvent.id">
+    <EventCard :myEvent="myEvent" />
+  </div>
+</template>
+
+<style scoped>
+.event-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
